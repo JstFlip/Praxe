1c0a883e363496d36a5f9769c9050d76b23a1811
JstFlip <58518901+JstFlip@users.noreply.github.com>
2020-01-14 21:03:33 +0100

    Add files via upload

--- Leity/index.js
+++ Leity/index.js
@@ -1,45 +1,27 @@
 //AOS//
 AOS.init();
-/*
-window.addEventListener("scroll", HeaderScrl);
-window.addEventListener("resize", BrowerResized);
+
+//Sticky NavBar #Header#//
 let header = document.getElementById("header");
-let subheader = document.getElementById("subheader");
-let section = document.getElementById("sec1");
-let scrld = header.offsetTop;
+let mobileHeader = document.getElementById("mobile-header");
+let offset = header.offsetTop;
 let width;
 
-function HeaderScrl() {
-  if (window.pageYOffset > scrld) {
-    if (width >= 811) {
-      header.classList.add("scrolled");
-      section.classList.remove("higherheight");
-      section.classList.add("lowerheight");
-    } else {
-      header.classList.add("scrolled");
-      subheader.classList.add("scrolled-sub");
-      section.classList.remove("higherheight");
-      section.classList.add("lowerheight");
-    }
-  } else {
-    header.classList.remove("scrolled");
-    subheader.classList.remove("scrolled-sub");
-    section.classList.remove("lowerheight");
-    section.classList.add("higherheight");
-  }
-}
-function BrowerResized() {
+function windowResize() {
   width = window.innerWidth;
-}*/
-
-window.addEventListener("scroll", ScrolledNav);
-let header = document.getElementById("header");
-let offset = header.offsetTop;
+}
+window.addEventListener("resize", windowResize);
 
 function ScrolledNav() {
   if (window.pageYOffset > offset) {
-    header.classList.add("scrolled");
+    if (width <= 900) {
+      mobileHeader.classList.add("scrolled");
+    } else {
+      header.classList.add("scrolled");
+    }
   } else {
     header.classList.remove("scrolled");
+    mobileHeader.classList.remove("scrolled");
   }
 }
+window.addEventListener("scroll", ScrolledNav);
